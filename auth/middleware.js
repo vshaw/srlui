d576a22978da39e742c743f6e32e7b7d1ade54bc
Vivienne Shaw <vshaw@wellesley.edu>
2019-09-29 10:19:56 -0400

    null check

--- auth/middleware.js
+++ auth/middleware.js
@@ -2,6 +2,9 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
   let token = req.headers['x-access-token'] || req.headers['authorization']; // Express headers are auto converted to lowercase
+  
+  if (!token) return res.status(401).send("Access denied. No token provided.");
+
   if (token.startsWith('Bearer ')) {
     // Remove Bearer from string
     token = token.slice(7, token.length);
