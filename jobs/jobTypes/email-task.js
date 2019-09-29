fd107147968d9e6d1fdb7fbcbcddb2864f605ab3
Vivienne Shaw <vshaw@wellesley.edu>
2019-09-29 01:10:02 -0400

    quit it

--- jobs/jobTypes/email-task.js
+++ jobs/jobTypes/email-task.js
@@ -7,12 +7,12 @@ const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAI
 module.exports = function(agenda) {
     agenda.define('email task', function(job, done) {
 
-    	console.log("in task"); 
         var data = job.attrs.data; 
 
         mg.messages().send(data, function (error, body) {
             console.log(body); 
         });
 
+        done(); 
     });
 };
