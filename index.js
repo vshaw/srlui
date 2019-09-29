0d5206fe8e4d4fc4691e2349b315ed6feae40676
Vivienne Shaw <vshaw@wellesley.edu>
2019-09-29 11:36:13 -0400

    allow headers

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ var port = process.env.PORT || 8080;
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Accept, X-CSRFToken, chap, seq, vert");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Accept, X-CSRFToken, x-access-token");
     res.header("Access-Control-Allow-Methods", "PUT, GET, POST");
     next();
 });
