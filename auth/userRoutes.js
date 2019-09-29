77261c9ec66a5beb210c4890e09e4caae4110c7d
Vivienne Shaw <vshaw@wellesley.edu>
2019-09-29 10:55:54 -0400

    fix auth

--- auth/userRoutes.js
+++ auth/userRoutes.js
@@ -1,7 +1,7 @@
 
-const auth = require("../middleware/auth");
+const auth = require("./middleware");
 const bcrypt = require("bcrypt");
-const { User, validate } = require("../models/user.model");
+const { User, validate } = require("./userModel");
 const express = require("express");
 const router = express.Router();
 
