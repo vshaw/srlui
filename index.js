88ecc42fe05ea7f630289de69abcf36cf55b5bb4
Vivienne Shaw <vshaw@wellesley.edu>
2019-10-08 17:49:21 -0400

    ah

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let app = express();
 
 let {PythonShell} = require('python-shell')
 
-PythonShell.run('scraping_scripts/discussionCrawler.py', function (err) {
+PythonShell.run('scraping_scripts/discussionCrawler.py', null, function (err) {
   if (err) throw err;
   console.log('discussion scraper finished');
 });
