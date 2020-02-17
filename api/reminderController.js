a974c7afe0001a4607f7bae73e9e9ed2b17ad8b1
vshaw <vshaw@wellesley.edu>
2020-02-17 09:55:59 -0500

    Update reminderController.js

--- api/reminderController.js
+++ api/reminderController.js
@@ -13,6 +13,7 @@ exports.new = async function (req, res) {
     reminder.email = req.body.email;
     reminder.weekNumber = req.body.weekNumber;
     reminder.weekId = req.body.weekId;
+    reminder.content = req.body.content;
 
     reminder.task1 = req.body.task1 ? req.body.task1 : null;
     reminder.date1 = req.body.date1 ? req.body.date1 : null;
@@ -25,6 +26,22 @@ exports.new = async function (req, res) {
     reminder.task3 = req.body.task3 ? req.body.task3 : null;
     reminder.date3 = req.body.date3 ? req.body.date3 : null;
     reminder.offset3 = req.body.offset3 ? req.body.offset3 : null;
+
+    reminder.task4 = req.body.task4 ? req.body.task4 : null;
+    reminder.date4 = req.body.date4 ? req.body.date4 : null;
+    reminder.offset4 = req.body.offset4 ? req.body.offset4 : null;
+
+    reminder.task5 = req.body.task5 ? req.body.task5 : null;
+    reminder.date5 = req.body.date5 ? req.body.date5 : null;
+    reminder.offset5 = req.body.offset5 ? req.body.offset5 : null;
+
+    reminder.task6 = req.body.task6 ? req.body.task6 : null;
+    reminder.date6 = req.body.date6 ? req.body.date6 : null;
+    reminder.offset6 = req.body.offset6 ? req.body.offset6 : null;
+
+    reminder.task7 = req.body.task7 ? req.body.task7 : null;
+    reminder.date7 = req.body.date7 ? req.body.date7 : null;
+    reminder.offset7 = req.body.offset7 ? req.body.offset7 : null;
 
     console.log(reminder); 
         
@@ -68,12 +85,43 @@ exports.new = async function (req, res) {
 
     if (reminder.date3 != null && reminder.task3 != null)
     {
-        var date = new Date(reminder.date1);
+        var date = new Date(reminder.date3);
 
         data.text = emailText1 + reminder.offset3 + emailText2 + reminder.task3;
         await agenda.schedule(date, 'email task', data);
     }
 
+    if (reminder.date4 != null && reminder.task4 != null)
+    {
+        var date = new Date(reminder.date4);
+
+        data.text = emailText1 + reminder.offset4 + emailText2 + reminder.task4;
+        await agenda.schedule(date, 'email task', data);
+    }
+
+    if (reminder.date5 != null && reminder.task5 != null)
+    {
+        var date = new Date(reminder.date5);
+
+        data.text = emailText1 + reminder.offset5 + emailText2 + reminder.task5;
+        await agenda.schedule(date, 'email task', data);
+    }
+
+    if (reminder.date6 != null && reminder.task6 != null)
+    {
+        var date = new Date(reminder.date6);
+
+        data.text = emailText1 + reminder.offset6 + emailText2 + reminder.task6;
+        await agenda.schedule(date, 'email task', data);
+    }
+
+        if (reminder.date7 != null && reminder.task7 != null)
+    {
+        var date = new Date(reminder.date7);
+
+        data.text = emailText1 + reminder.offset7 + emailText2 + reminder.task7;
+        await agenda.schedule(date, 'email task', data);
+    }
 };
 
 exports.index = function (req, res) {
