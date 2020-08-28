9977bcdbe2cc370dc6f50389ad4c8392ccd7b1ab
Vivienne <vshaw@wellesley.edu>
2020-08-28 01:45:03 -0400

    um

--- api/activityController.js
+++ api/activityController.js
@@ -39,7 +39,7 @@ exports.saveGoals = function (req, res) {
     var update = 
     {
         '$set': {
-           'goals.$.15.$.videoGoal': req.query.videoGoal, 
+           'goals.$.15.$.videoGoal': 2, 
         }
     };
 
