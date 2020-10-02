5a4bad8d804875bf992beece4402d9561379ac1c
Vivienne <vshaw@wellesley.edu>
2020-10-02 19:04:22 -0400

    ho hum

--- api/activityController.js
+++ api/activityController.js
@@ -64,7 +64,10 @@ exports.editActivity = function (req, res) {
     // This is so hacky but I'm desparate
     var weekNumberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
+    console.log(weekNumberArray); 
     var index = weekNumberArray.indexOf(weekNumber);
+
+    console.log("index: " + index); 
     weekNumberArray.splice(index, 1);
 
     console.log(weekNumberArray);
