b063a35910ee948217ada33ef65ab77a4336d5c9
Vivienne <vshaw@wellesley.edu>
2020-12-10 18:39:34 -0500

    course info sample file

---
+++ edX_modules/js/SRLUICourseInfo.js
@@ -0,0 +1,363 @@
+var mappedInfo = {
+
+    0 : {
+        "topics": [
+        "1. Learning to use Weekly Review",
+        "2. Start planning for study",
+        "3. Review all topics of the course"
+        ],
+        "tip": "Start a habit of planning and setting time for study helps you get a great start of the course",
+        "pic": "managingyourstudy.png"
+    },
+
+    1 : {
+        "topics": [
+        "1. Course Introduction",
+        "2. Marketing Framework",
+        "3. Mini Cases"        
+
+        ],
+        "tip": "Keep good notes and share your key takeaways with your classmates!", 
+        "pic": "takegoodnotes.png"
+    },
+
+    2 : {
+        "topics": [
+        "1. Market Segmentation Basics",
+        "2. Hierarchical Clustering",
+        "3. K-Means Clustering",
+        "4. Latent Class Analysis"
+        ],
+        "tip": "Plan your week ahead and stick to your schedule. But, don't forget to take a break to reward yourself! ",
+        "pic": "TAKEABREAK.png"
+    },
+
+    3 : {
+        "topics": [
+        "1. Perceptual Mapping",
+        "2. Foundation of Factor Analysis",
+        "3. Factors Analysis with R",
+        "4. Application: Understanding Preferences for Cars",
+        "5. Application: Drivers of Brand Preference and Conclusion"
+        ],
+        "tip": "Avoid watching a lot of lectures all at once. Break it down into smaller sessions so you don't overwhelm your brain. ",
+        "pic": "brain.png"
+    },
+
+    4 : {
+        "topics": [
+        "1. Intro to Choice Modelling",
+        "2. Logistics Regression",
+        "3. Multinomial Logit (MNL)",
+        "4. Conclusion"
+        ],
+        "tip": "Eliminate distractions. Turn off your phones, TV, and close social media. You can \"like\" that puppy picture later! ", 
+        "pic": "distraction.png"
+    },
+
+    5 : {
+        "topics": [
+        "1. Customer Relationship Management (CRM) Introduction",
+        "2. Customer Liftime Value (CLV)",
+        "3. CLV Formula",
+        "4. Margin Multiple",
+        "5. Example: Customer Acquisition",
+        "6. Increase Customer Value Strategy",
+        "7. Example: A Channel Story",
+        "8. Customer Expansion Introduction",
+        "9. Customer Recommendation",
+        "10. Matrix Factorization",
+        "11. Matrix Factorization in R",
+        "12. Matrix Factorization in R (Part 2)",
+        "13. Customer Retention"
+        ],
+        "tip": "If you need help, you can reach out to the course staff or talk to your classmates on the discussion forum. You can also review the discussion forum to see if your questions have been answered. ", 
+        "pic": "discussion_forum.png"
+    },
+
+    6 : {
+        "topics": [
+        "1. Introduction - Types Of New Products",
+        "2. Why Good Ideas Go Bad. New Product Funnel",
+        "3. Idea Generation and Design",
+        "4. Test Market",
+        "5. Simulated Test Market",
+        "6. Pre-market Testing: Assessor Model",
+        "7. Trial Calculation",
+        "8. Repeat Calculation",
+        "9. Repeat Formula: Derivation and Intuition",
+        "10. Interpretation Of Results And Conclusion"
+        ],
+        "tip": "Find a perfect study spot, like a library or a coffee shop, preferably with a free refill! ",
+        "pic": "studyspot.png"
+    },
+
+    7 : {
+        "topics": [
+        "1. Introduction and Product Design Task",
+        "2. Conjoint Analysis Definition",
+        "3. Conjoint Analysis Stages",
+        "4. Identifying Attributes and Levels - Selecting Product Profiles",
+        "5. Preference Data, Data Collection, and Overview of Data Analysis",
+        "6. Dummy Variable Coding and Regression Output",
+        "7. Part-Worth Plots, Relative Importance Weights, and Ideal/Worst Product",
+        "8. Predictions and Market Share and Conjoint Summary"
+        ],
+        "tip": "Review the syllabus and keep track of the due dates. And set small deadlines for yourself to finish the assignments early!",
+        "pic": "deadline.png"
+    },
+    8 : {
+        "topics": [
+        "1. Product Line Optimization Overview",
+        "2. Product Line Optimization with Ratings-Based Conjoint-Introduction",
+        "3. Data, Choice Process and Firm Profits",
+        "4. Firm Problem",
+        "5. R Code: Profit Objective Function Formulation",
+        "6. R Code: Profit Optimization Using Genetic Algorithms",
+        "7. Concluding remars on ratings-based conjoint for product line optimization",
+        "8. Choice-Based Conjoint Introduction",
+        "9. MNL Model Structure and Partworths",
+        "10. MNL Estimation",
+        "11. Choice Prediction and Conjoint Simulator",
+        "12. Attribute Pricing",
+        "13. Conclusion"
+        ],
+        "tip": "Build your learning community and interact with your classmates, they might have similar questions as you do.",
+        "pic": "community.png"
+    },
+
+    9 : {
+        "topics": [
+        "1. Introduction",
+        "2. Price Optimization using WTP and WTP Distribution",
+        "3. Normal Distribution and Demand Curve",
+        "4. Uniform WTP Distribution",
+        "5. Logistic WTP, Methods of Moments, and Maximum Likelihood Estimation",
+        "6. Price Optimization with Logistic WTP in R",
+        "7. Uncertainty and Bootstrapping Demand Curves"
+        ],
+        "tip": "You can download the videos offline and watch them on your phone. It is a great way to review the lecture videos!",
+        "pic": "download.png"
+    },
+
+    10 : {
+        "topics": [
+        "1. Price Optimization Introduction",
+        "2. Modeling Framework",
+        "3. Regression",
+        "4. Logistic Regression",
+        "5. Logistic Regression in R",
+        "6. Prospect theory",
+        "7. Reference Price and Case study",
+        "8. B2B Bidding in R",
+        "9. Summary"
+        ],
+        "tip": "Plan your week ahead and stick to your schedule. But, don't forget to take a break to reward yourself! ",
+        "pic": "TAKEABREAK.png"
+    },
+
+    11 : {
+        "topics": [
+        "1. Advertising Overview",
+        "2. Fruitazia Case- Test Market Design and Raw Data",
+        "3. Exploratory Analysis",
+        "4. Basic Regression Analysis",
+        "5. Regression Analysis with Interaction Term",
+        "6. Fruitazia Case- Overview of Results",
+        "7. Bazaar.com Case- Paid Search Dilemma",
+        "8. Natural experiment- Data and Naive ROI Analysis",
+        "9. Correction for Seasonality, Breakeven Cost-per-Click",
+        "10. Conclusion"
+        ],
+        "tip": "Ok! We know the course is challenging. But, if you are stressed, take a break and come back. You can do it!",
+        "pic": "schedule.png"
+    },
+
+    12 : {
+        "topics": [
+        "1. Introduction",
+        "2. Short Run Impact of Sales Promotion",
+        "3. How Promotion Affect Sales",
+        "4. Data Visualization",
+        "5. Statistical Inference",
+        "6. Output Interpretation and Conclusion"
+        ],
+        "tip": "To prepare for your final exam, review your notes, study guides, quizzes and assignments. Good luck with your final!", 
+        "pic": "congrats.png"
+    }
+};
+
+var courseContent = [
+    [ 1, "Course Introduction and Agenda", "1:04"],
+    [ 1, "What is Marketing?", "3:01"],
+    [ 1, "Strategic Marketing Framework", "3:27"],
+    [ 1, "Outline of the Course", "0:42"],
+    [ 1, "Course Overview Part I", "6:09"],
+    [ 1, "Course Overview Part II", "6:14"],
+    [ 1, "Course Requirements", "0:49"],
+    [ 1, "Understanding Customers", "9:26"],
+    [ 1, "Economic Value to Customers (EVC)", "4:16"],
+    [ 1, "Examples of EVC", "12:13"],
+    [ 1, "Issues of EVC and Perceived Value", "8:37"],
+    [ 1, "Mini-Case A: Designing a Rock Climbing Harness", "10:02"],
+    [ 1, "Mini-Case A: Comparison to Competitors", "5:48"],
+    [ 1, "Mini-Case B: Alpha Minicomputer", "8:47"],
+    [ 1, "Week 1 Quiz", "20:00", 10],
+
+    [ 2, "Market Segmentation Basics", "8:33"],
+    [ 2, "Market Segmentation Bases", "7:58"],
+    [ 2, "Market Segmentation Examples (Part 1)", "5:03"],
+    [ 2, "Market Segmentation Examples (Part 2)", "10:42"],
+    [ 2, "Hierarchical Clustering Example (Part 1)", "4:50"],
+    [ 2, "Hierarchical Clustering Example (Part 2)", "5:58"],
+    [ 2, "Hierarchical Clustering Example (Part 3)", "7:00"],
+    [ 2, "A Marketing Application on R", "11:17"],
+    [ 2, "How to Choose the Number of Segments (Part 1)", "7:16"],
+    [ 2, "How to Choose the Number of Segments (Part 2)", "10:10"],
+    [ 2, "K-Means Clustering", "11:47"],
+    [ 2, "Latent Class Analysis", "11:52"],
+    [ 2, "Week 2 Quiz", "20:00", 10],
+    [ 2, "Week 2 Assignment", "60:00"],
+
+    [ 3, "Perceptual Mapping", "9:35"],
+    [ 3, "Foundation of Factor Analysis (Part 1)", "7:17"],
+    [ 3, "Foundation of Factor Analysis (Part 2)", "7:02"],
+    [ 3, "Determining the Number of Factors", "9:01"],
+    [ 3, "Factors Structure", "5:28"],
+    [ 3, "Interpreting the Factors", "11:37"],
+    [ 3, "Determining the Number of Factors", "11:34"],
+    [ 3, "Application: Drivers of Brand Preference and Conclusion", "10:58"],
+    [ 3, "Week 3 Quiz", "20:00", 10],
+    [ 3, "Week 3 Assignment", "60:00"],
+
+    [ 4, "Introduction (Part 1)", "5:07"],
+    [ 4, "Introduction (Part 2)", "7:52"],
+    [ 4, "Motivation", "10:07"],
+    [ 4, "Mechanics: Utilities and Probabilities", "8:04"],
+    [ 4, "Estimation and Output Interpretation", "11:08"],
+    [ 4, "Applications I: Classification and Classifier Quality (Part 1)", "16:51"],
+    [ 4, "Applications I: Classification and Classifier Quality (Part 2)", "6:24"],
+    [ 4, "Applications II: Scenario Simulation, Lift Calculation, and Targeting", "10: 25"],
+    [ 4, "Introduction Data and Research Questions", "11:55"],
+    [ 4, "MNL model with Product Attributes", "8:27"],
+    [ 4, "Estimation and Interpretation", "7:16"],
+    [ 4, "Model Fit: Likelihood-based", "9:36"],
+    [ 4, "Model Fit: Prediction-Based", "8:50"],
+    [ 4, "Incorporating Demographic Variables", "8:27"],
+    [ 4, "Interacting Product Attributes and Demographic Variables", "4:22"],
+    [ 4, "Conclusion", "4:56"],
+    [ 4, "Week 4 Quiz", "20:00", 10],
+
+    [ 5, "Customer Relationship Management (CRM) Introduction", "10:03"],
+    [ 5, "Customer Liftime Value (CLV)", "10:08"],
+    [ 5, "CLV Formula", "15:39"],
+    [ 5, "Margin Multiple", "4:37"],
+    [ 5, "Example- Customer Acquisition", "15:08"],
+    [ 5, "Increase Customer Value Strategy", "7:49"],
+    [ 5, "Example- A Channel Story", "3:44"],
+    [ 5, "Customer Expansion Introduction", "3:42"],
+    [ 5, "Customer Recommendation", "5:03"],
+    [ 5, "Matrix Factorization", "9:54"],
+    [ 5, "Matrix Factorization in R", "5:32"],
+    [ 5, "Matrix Factorization in R (Part 2)", "11:7"],
+    [ 5, "Customer Retention", "8:04"],
+    [ 5, "Week 5 Quiz", "20:00", 10],
+
+    [ 6, "Introduction - Types Of New Products", "13:00"],
+    [ 6, "Why Good Ideas Go Bad. New Product Funnel", "8:25"],
+    [ 6, "Idea Generation and Design", "7:08"],
+    [ 6, "Test Market", "6:01"],
+    [ 6, "Simulated Test Market", "7:49"],
+    [ 6, "Pre-market Testing: Assessor Model", "6:56"],
+    [ 6, "Trial Calculation", "13:27"],
+    [ 6, "Repeat Calculation", "11:07"],
+    [ 6, "Repeat Formula: Derivation and Intuition", "9:10"],
+    [ 6, "Interpretation Of Results And Conclusion", "4:07"],
+    [ 6, "Week 6 Quiz", "20:00", 9],
+
+    [ 7, "Introduction and Product Design Task", "12:48"],
+    [ 7, "Conjoint Analysis Definition", "12:23"],
+    [ 7, "Conjoint Analysis Stages", "5:42"],
+    [ 7, "Identifying Attributes and Levels - Selecting Product Profiles", "10:15"],
+    [ 7, "Preference Data, Data Collection, and Overview of Data Analysis", "7:36"],
+    [ 7, "Dummy Variable Coding and Regression Output", "11:32"],
+    [ 7, "Part-Worth Plots, Relative Importance Weights, and Ideal/Worst Product", "9:06"],
+    [ 7, "Predictions and Market Share and Conjoint Summary", "10:29"],
+    [ 7, "Week 7 Quiz", "20:00", 10],
+
+    [ 8, "Product Line Optimization Overview", "4:25"],
+    [ 8, "Product Line Optimization with Ratings-Based Conjoint-Introduction", "2:43"],
+    [ 8, "Data, Choice Process and Firm Profits", "8:01"],
+    [ 8, "Firm Problem", "4:12"],
+    [ 8, "R Code: Profit Objective Function Formulation", "8:38"],
+    [ 8, "R Code: Profit Optimization Using Genetic Algorithms", "6:45"],
+    [ 8, "Concluding remars on ratings-based conjoint for product line optimization", "1:22"],
+    [ 8, "Choice-Based Conjoint Introduction", "6:47"],
+    [ 8, "MNL Model Structure and Partworths", "10:26"],
+    [ 8, "MNL Estimation", "6:56"],
+    [ 8, "Choice Prediction and Conjoint Simulator", "3:44"],
+    [ 8, "Attribute Pricing", "6:35"],
+    [ 8, "Conclusion", "2:26"],
+    [ 8, "Week 8 Quiz", "20:00", 10],
+    [ 8, "Week 8 Assignment", "60:00"],
+
+    [ 9, "Introduction", "13:25"],
+    [ 9, "Price Optimization using WTP and WTP Distribution", "9:35"],
+    [ 9, "Normal Distribution and Demand Curve", "12:03"],
+    [ 9, "Uniform WTP Distribution", "6:43"],
+    [ 9, "Logistic WTP, Methods of Moments, and Maximum Likelihood Estimation", "12:48"],
+    [ 9, "Price Optimization with Logistic WTP in R", "8:48"],
+    [ 9, "Uncertainty and Bootstrapping Demand Curves", "8:46"],
+    [ 9, "Week 9 Quiz", "40:00", 10],
+
+    [ 10, "Price Optimization Introduction", "13:25"],
+    [ 10, "Modeling Framework", "10:07"],
+    [ 10, "Regression", "9:34"],
+    [ 10, "Logistic Regression", "5:49"],
+    [ 10, "Logistic Regression in R", "9:28"],
+    [ 10, "Prospect theory", "3:35"],
+    [ 10, "Reference Price and Case study", "7:59"],
+    [ 10, "B2B Bidding in R", "7:21"],
+    [ 10, "Summary", "3:25"],
+    [ 10, "Week 10 Quiz", "30:00", 10],
+    [ 10, "Week 10 Assignment", "70:00"],
+
+    [ 11, "Advertising Overview", "6:44"],
+    [ 11, "Fruitazia Case- Test Market Design and Raw Data", "8:27"],
+    [ 11, "Exploratory Analysis", "10: 06"],
+    [ 11, "Basic Regression Analysis", "13:50"],
+    [ 11, "Regression Analysis with Interaction Term", "10:55"],
+    [ 11, "Fruitazia Case- Overview of Results", "1:44"],
+    [ 11, "Bazaar.com Case- Paid Search Dilemma", "13:30"],
+    [ 11, "Natural experiment- Data and Naive ROI Analysis", "9:15"],
+    [ 11, "Correction for Seasonality, Breakeven Cost-per-Click", "9:33"],
+    [ 11, "Conclusion", "3:46"],
+    [ 11, "Week 11 Quiz", "30:00", 10],
+
+    [ 12, "Introduction", "13:11"],
+    [ 12, "Short Run Impact of Sales Promotion", "10:27"],
+    [ 12, "How Promotion Affect Sales", "9:56"],
+    [ 12, "Data Visualization", "11:23"],
+    [ 12, "Statistical Inference", "9:23"],
+    [ 12, "Output Interpretation and Conclusion", "12:37"],
+    [ 12, "Week 12 Quiz", "40:00", 10]
+];
+
+DATES = {
+    0 : '2020-09-21',
+    1 : '2020-09-27',
+    2 : '2020-10-04',
+    3 : '2020-10-11',
+    4 : '2020-10-18',
+    5 : '2020-10-25',
+    6 : '2020-11-01',
+    7 : '2020-11-08',
+    8 : '2020-11-15',
+    9 : '2020-11-22',
+    10: '2020-11-29',
+    11: '2020-12-06',
+    12: '2020-12-13',
+    13: '2020-12-20'
+}; 
+
+var accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZDkwYzYxODllOGU3MzAwMDQ2NDUxMTMiLCJpYXQiOjE1Njk3Njg5ODV9.B0667naGxkMGg3y0n1RSxQh8ucVEKkeLcgIXkuY3r6M";
